9eab0d0d4f7e1e0966dc286fea1185d5e3354781
Gareth Evans <gareth@garethevans.com>
2018-06-06 06:58:10 +0100

    almost got the first test to pass

---
+++ SymbolSource.Contract.Tests/AwsStorageServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using SymbolSource.Contract.Storage;
+using SymbolSource.Contract.Storage.Aws;
+
+namespace SymbolSource.Contract.Tests
+{
+    public class AwsStorageServiceTests : StorageServiceTests
+    {
+        public AwsStorageServiceTests()
+        {
+            Storage = new AwsStorageService();
+        }
+
+        protected override IStorageService Storage { get; }
+    }
+
+
+}
--- SymbolSource.Contract/Storage/AwsStorageService.cs
+++ SymbolSource.Contract/Storage/AwsStorageService.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Runtime.Internal;
 using Amazon.S3;
+using Amazon.S3.Model;
 
-namespace SymbolSource.Contract.Storage
+namespace SymbolSource.Contract.Storage.Aws
 {
     public class AwsStorageService : IStorageService
     {
@@ -12,25 +20,55 @@ namespace SymbolSource.Contract.Storage
         private const string NamedFeedPrefix = "named-";
         private const string DefaultFeedName = "default";
 
-        private static readonly RegionEndpoint BucketRegion = RegionEndpoint.EUWest1;
+        private static readonly RegionEndpoint Region = RegionEndpoint.EUWest1;
         private static IAmazonS3 _s3Client;
 
         public AwsStorageService()
         {
-            _s3Client = new AmazonS3Client(BucketRegion);
+            _s3Client = new AmazonS3Client(new AmazonS3Config
+            {
+                RegionEndpoint = Region
+            });
         }
 
         public IStorageFeed GetFeed(string feedName)
         {
             var bucketName = GetBucketName(feedName);
+            var tableName = GetTableName(bucketName);
 
-            _s3Client.
-            throw new System.NotImplementedException();
+            var dynamoConfig = new AmazonDynamoDBConfig
+            {
+                RegionEndpoint = Region
+            };
+
+            var s3Config = new AmazonS3Config
+            {
+                RegionEndpoint = Region
+            };
+
+            var packageTable = new PackageTable(dynamoConfig, tableName);
+
+            return new AwsStorageFeed(bucketName, packageTable, bucketName, s3Config, dynamoConfig);
         }
 
         public IEnumerable<string> QueryFeeds()
         {
-            throw new System.NotImplementedException();
+            var bucketsResponse = _s3Client.ListBuckets();
+
+            return bucketsResponse.Buckets
+                .Select(container => container.BucketName)
+                .Where(name => name.StartsWith(FeedPrefix))
+                .Select(name => name.Substring(FeedPrefix.Length))
+                .Select(name =>
+                {
+                    if (name == DefaultFeedName)
+                        return null;
+
+                    if (name.StartsWith(NamedFeedPrefix))
+                        return name.Substring(NamedFeedPrefix.Length);
+
+                    throw new ArgumentOutOfRangeException();
+                });
         }
 
         private static string GetBucketName(string feedName)
@@ -49,12 +87,28 @@ namespace SymbolSource.Contract.Storage
             feedName = FeedPrefix + feedName;
             return feedName;
         }
+
+        private static readonly Regex TableRegex = new Regex("[^A-Za-z0-9]", RegexOptions.Compiled);
+
+        private static string GetTableName(string containerName)
+        {
+            return TableRegex.Replace(containerName, "");
+        }
     }
 
     public class AwsStorageFeed : IStorageFeed
     {
-        public AwsStorageFeed(string feedName)
+        private readonly PackageTable _table;
+        private readonly string _bucketName;
+        private readonly AmazonS3Config _s3Config;
+        private readonly AmazonDynamoDBConfig _dynamoConfig;
+
+        public AwsStorageFeed(string feedName, PackageTable table, string bucketName, AmazonS3Config s3Config, AmazonDynamoDBConfig dynamoConfig)
         {
+            _table = table;
+            _bucketName = bucketName;
+            _s3Config = s3Config;
+            _dynamoConfig = dynamoConfig;
             Name = feedName;
         }
 
@@ -62,7 +116,12 @@ namespace SymbolSource.Contract.Storage
 
         public IEnumerable<string> QueryInternals()
         {
-            throw new NotImplementedException();
+            using (var client = new AmazonS3Client(_s3Config))
+            {
+                var objects = client.ListObjects(_bucketName);
+
+                return objects.S3Objects.Select(o => string.Join("/", o.Key.Split('/').Skip(2)));
+            }
         }
 
         public Task<IEnumerable<PackageName>> QueryPackages(PackageState packageState)
@@ -72,7 +131,7 @@ namespace SymbolSource.Contract.Storage
 
         public Task<IEnumerable<PackageName>> QueryPackages(string userName, PackageState packageState)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(Enumerable.Empty<PackageName>());
         }
 
         public Task<IEnumerable<PackageName>> QueryPackages(PackageState packageState, string packageNamePrefix, int skip, int take)
@@ -87,7 +146,7 @@ namespace SymbolSource.Contract.Storage
 
         public IPackageStorageItem GetPackage(string userName, PackageState packageState, PackageName packageName)
         {
-            throw new NotImplementedException();
+            return new AwsPackageStorageItem(this, packageName, packageState, _table, _bucketName, _s3Config, _dynamoConfig, userName);
         }
 
         public IPackageRelatedStorageItem GetSymbol(PackageName packageName, SymbolName symbolName)
@@ -105,4 +164,304 @@ namespace SymbolSource.Contract.Storage
             throw new NotImplementedException();
         }
     }
+
+    public class AwsPackageStorageItem : IPackageStorageItem
+    {
+        private readonly PackageTable _table;
+        private readonly string _bucketName;
+        private readonly PackageBucket _bucket;
+        private readonly AmazonS3Config _s3Config;
+        private readonly AmazonDynamoDBConfig _dynamoConfig;
+        private readonly string _username;
+
+        public AwsPackageStorageItem(
+            IStorageFeed feed,
+            PackageName name,
+            PackageState state,
+            PackageTable table,
+            string bucketName,
+            AmazonS3Config s3Config,
+            AmazonDynamoDBConfig dynamoConfig,
+            string username)
+        {
+            _table = table;
+            _bucketName = bucketName;
+            _s3Config = s3Config;
+            _dynamoConfig = dynamoConfig;
+            _username = username;
+            _bucket = new PackageBucket(bucketName, s3Config);
+
+            Feed = feed;
+            Name = name;
+            State = state;
+        }
+
+        public IStorageFeed Feed { get; }
+
+        public bool CanGetUri => false;
+
+        public async Task<bool> Exists()
+        {
+            if (!await _bucket.Exists()) return false;
+
+            using (var client = new AmazonS3Client(_s3Config))
+            {
+                var key = GetPath(State, _username, Name);
+
+                var response = await client.ListObjectsAsync(_bucketName, key);
+
+                return response.S3Objects.SingleOrDefault(x => x.Key == key) != null;
+            }
+        }
+
+        public Task<Uri> GetUri()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Stream> Get()
+        {
+            using (var client = new AmazonS3Client(_s3Config))
+            {
+                try
+                {
+                    var obj = await client.GetObjectAsync(_bucketName, GetPath(State, _username, Name));
+
+                    return obj.ResponseStream;
+                }
+                catch (AmazonS3Exception ex) when (ex.ErrorCode == "NoSuchBucket" || ex.ErrorCode == "NoSuchKey")
+                {
+                    return null;
+                } 
+            }
+        }
+
+        public async Task<Stream> Put()
+        {
+            using (var client = new AmazonS3Client(_s3Config))
+            {
+                await client.PutBucketAsync(_bucketName);
+            }
+
+            return new WriteS3ObjectOnDisposeStream(_s3Config, _bucketName, GetPath(State, _username, Name));
+        }
+
+        public Task Get(Stream target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Put(Stream source)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Delete()
+        {
+            //delete blob
+
+            if (!await Exists()) return false;
+
+            try
+            {
+                using (var client = new AmazonS3Client(_s3Config))
+                {
+                    var response = await client.DeleteObjectAsync(_bucketName, GetPath(State, _username, Name));
+
+                    return response.HttpStatusCode == HttpStatusCode.NoContent;
+                }
+            }
+            catch (AmazonS3Exception ex) when (ex.ErrorCode == "NoSuchBucket" || ex.ErrorCode == "NoSuchCode")
+            {
+                return false;
+            }
+
+            //delete dynamo row
+
+            return false;
+        }
+
+        public PackageName Name { get; }
+
+        public PackageState State { get; }
+
+        public async Task<string> GetUserName()
+        {
+            var packageEntity = await _table.RetrieveAsync(State, Name);
+
+            if (packageEntity != null)
+            {
+                if (_username == null)
+                    return packageEntity["username"];
+
+                if (packageEntity["username"] != _username)
+                    return null;
+            }
+
+            return _username;
+        }
+
+        public Task<IPackageStorageItem> Move(PackageState newState, PackageName newName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IPackageStorageItem> Copy(PackageState newState, PackageName newName)
+        {
+            if (!await Exists())
+                return null;
+
+            var newItem = await PrepareMoveOrCopy(newState, newName);
+            var packageEntity = await _table.RetrieveAsync(State, Name);
+            await newItem._table.InsertOrReplaceAsync(newState, newName, packageEntity);
+
+            var username = await GetUserName();
+
+            using (var client = new AmazonS3Client(_s3Config))
+            {
+
+                var request = new CopyObjectRequest
+                {
+                    SourceBucket = this.Feed.Name,
+                    SourceKey = GetPath(State, username, Name),
+                    DestinationBucket = this.Feed.Name,
+                    DestinationKey = GetPath(newState, username, newName)
+                };
+
+                CopyObjectResponse response = await client.CopyObjectAsync(request);
+            }
+
+            return newItem;
+        }
+
+        private static string GetDirectoryPath(PackageState packageState, string userName)
+        {
+            return string.Format("pkg/{0}/{1}", packageState.ToString().ToLower(), userName);
+        }
+
+        private static string GetPackagePath(PackageName packageName)
+        {
+            return string.Format("{0}/{1}/{0}.{1}.nupkg", packageName.Id, packageName.Version);
+        }
+
+        private static string GetPath(PackageState packageState, string userName, PackageName packageName)
+        {
+            return string.Format("{0}/{1}", GetDirectoryPath(packageState, userName), GetPackagePath(packageName));
+        }
+
+        private Task<AwsPackageStorageItem> PrepareMoveOrCopy(PackageState newState, PackageName newName)
+        {
+            var newPackage = new AwsPackageStorageItem(Feed, newName, newState, _table, _bucketName, _s3Config, _dynamoConfig, _username);
+
+            return Task.FromResult(newPackage);
+        }
+    }
+
+    public class PackageTable
+    {
+        private readonly AmazonDynamoDBConfig _config;
+        private readonly string _tableName;
+
+        public PackageTable(AmazonDynamoDBConfig config, string tableName)
+        {
+            _config = config;
+            _tableName = tableName;
+        }
+
+        public async Task<Document> RetrieveAsync(PackageState packageState, PackageName packageName)
+        {
+            var partitionKey = GetPartitionKey(packageState);
+            var rangeKey = GetRangeKey(packageName);
+
+            using (var client = new AmazonDynamoDBClient(_config))
+            {
+                var table = Table.LoadTable(client, _tableName);
+                var document = await table.GetItemAsync(partitionKey, rangeKey);
+
+                return document;
+            }
+        }
+
+        private static string GetPartitionKey(PackageState packageState)
+        {
+            return $"pkg*{packageState.ToString().ToLower()}";
+        }
+
+        private static string GetRangeKey(PackageName packageName)
+        {
+            return $"{packageName.Id}*{packageName.Version}";
+        }
+
+        public Task InsertOrReplaceAsync(PackageState newState, PackageName newName, Document packageEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PackageBucket
+    {
+        private readonly AmazonS3Config _config;
+
+        public PackageBucket(string bucketName, AmazonS3Config config)
+        {
+            Name = bucketName;
+            _config = config;
+        }
+
+        public string Name { get; }
+
+        public async Task<bool> Exists()
+        {
+            using (var client = new AmazonS3Client(_config))
+            {
+                var response = await client.ListBucketsAsync();
+                var bucket = response
+                    .Buckets
+                    .SingleOrDefault(b => b.BucketName == Name);
+
+                return bucket != null;
+            }
+        }
+    }
+
+    public class WriteS3ObjectOnDisposeStream : MemoryStream
+    {
+        private readonly AmazonS3Config _s3Config;
+        private readonly string _bucketName;
+        private readonly string _path;
+        private bool _disposed = false;
+
+        public WriteS3ObjectOnDisposeStream(AmazonS3Config s3Config, string bucketName, string path)
+        {
+            _s3Config = s3Config;
+            _bucketName = bucketName;
+            _path = path;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && !_disposed)
+            {
+                Seek(0, SeekOrigin.Begin);
+
+                using (var client = new AmazonS3Client(_s3Config))
+                {
+                    var request = new PutObjectRequest
+                    {
+                        BucketName = _bucketName,
+                        AutoCloseStream = false,
+                        Key = _path,
+                        InputStream = this
+                    };
+
+                    var response = client.PutObject(request);
+
+                    _disposed = true;
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+
 }
